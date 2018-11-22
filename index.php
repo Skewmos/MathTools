7c81f13076e59cda54c9af6671833b3ab8a0146b
LordDGT <jeremielegrand021@gmail.com>
2018-11-22 19:11:15 +0100

    Squelette de base
    Création du squelette html et importation du style . 
    base du style bootswatch  .

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MathTools</title>
+		<meta charset="utf-8">
+		
+		<!--Bootswatch-->
+		<link href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/lux/bootstrap.min.css" rel="stylesheet" integrity="sha384-ML9h/UCooefre72ZPxxOHyjbrLT1xKV0AHON1J+OlOV2iwcYemqmWyMfTcfyzLJ1" crossorigin="anonymous">
+
+		<!--Style css -->
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div class="container">
+				<a class="navbar-brand" href="#">MathTools</a>
+				
+			  	<div class="collapse navbar-collapse" id="navbarColor01">
+				    <ul class="navbar-nav ml-auto">
+				      	<li class="nav-item">
+				       		<a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+				      	</li>
+				      	<li class="nav-item">
+				        	<a class="nav-link" href="#">Outils</a>
+				      	</li>
+				      	<li class="nav-item">
+				        	<a class="nav-link" href="#">A propos</a>
+				      	</li>
+				      	<li class="nav-item">
+				        	<a class="nav-link" href="#">Contact</a>
+				      	</li>
+				    </ul>
+			  	</div>
+		  	</div>
+		</nav>
+	
+		<header>	
+			<div  class="jumbotron" id="MAJ">
+				<h1 class="display-3">MathTools</h1>
+				<hr class="my-4">
+				<p class="entete">Qu'est-ce que c'est MathTools ? Des Mathématiques ? Un outil ?</p><br>
+				<p class="normal">Nous vous proposons ici même de vérifier, ou de vous apporter une aide mathématiques pour vos calculs. Vous trouverez tout types d'outils sur des calculs precis, tout comme le calcul de delta (&#916) par exemple.</p>
+			</div>
+		</header>
+
+		<div class="container">
+			<h2 class="titleh2">Nos outils :</h2><br>
+			<div  class="row">
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">Cet outil vous sera très utile si vous travaillez sur des calculs assez simple, il vous permet un gain de temps</p>
+  						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">Nous vous proposons ici un outil permettant les fonctionnalités suivantes :</p>
+  						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">Nous vous proposons par ici un outil permettant de vérifier les racines de votre fonction du second degré.</p>
+  						</div>
+					</div>
+				</div>
+				<div class="space"></div>
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">A propos de l'outil</p>
+  						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">A propos de l'outil</p>
+  						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-4">
+					<div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+  						<div class="card-header">Thème de l'outil</div>
+  						<div class="card-body">
+	    					<h4 class="card-title">Nom de l'outil</h4>
+	    					<p class="card-text">A propos de l'outil</p>
+  						</div>
+					</div>
+				</div>
+				<div class="space"></div>
+			</div>
+		</div>
+
+		<footer>
+			<div class="container">
+				
+			</div>
+		</footer>
+
+		<div class="footsite">
+			<div class="container">
+				<p class="copy">&copy; - Copyright 2018</p>
+			</div>
+		</div>
+	</body>
+</html>
